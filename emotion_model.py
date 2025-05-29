795e2cf94fe81f4650207371b9d09f4a52e6ac6c
DmitrySonora <dksonora@gmail.com>
2025-05-30 02:27:17 +0300

    Update emotion_model.py

--- emotion_model.py
+++ emotion_model.py
@@ -1,1 +1,5 @@
+from transformers import pipeline
+
+# Для ускорения и предотвращения повторной загрузки — инициализируем один раз при старте
+emotion_classifier = pipeline("text-classification", model="cointegrated/rubert-tiny2-cedr-emotion-detection", top_k=None)
 
